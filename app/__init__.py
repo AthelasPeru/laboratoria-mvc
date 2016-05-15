2b6d2f7692491363fb38bb3aa4f169249d0552a8
Pedro Baumann <ondoheer@gmail.com>
2016-05-15 10:53:49 -0500

    Finalizao app básico

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from flask_zurb_foundation import Foundation
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+
+
+
+from app.models import db
+from app.models.student import Student
+from app.models.skill import Skill
+from app.models.company import Company
 
 from blueprints.frontend import frontend
-from blueprints.admin import admin
+from blueprints.admin import AdminStudentView, AdminCompanyView
+
 
 
 def create_app(app=None, config_file=None):
@@ -17,9 +28,15 @@ def create_app(app=None, config_file=None):
 
     # Blueprints
     app.register_blueprint(frontend)
-    app.register_blueprint(admin)
+    
 
     # Extensions
     Foundation(app)
+    admin = Admin(app, name='Bolsa de Trabajo', template_mode='bootstrap3')
+
+    # Admin Views
+    admin.add_view(ModelView(Skill, db.session))
+    admin.add_view(AdminStudentView(Student, db.session))
+    admin.add_view(AdminCompanyView(Company, db.session))
 
     return app
--- app/blueprints/frontend.py
+++ app/blueprints/frontend.py
@@ -4,6 +4,7 @@ from flask import Blueprint, render_template, abort
 from app.models import db
 from app.models.student import Student
 from app.models.company import Company
+from app.models.skill import Skill
 
 frontend = Blueprint("front", __name__, template_folder="views")
 
@@ -30,22 +31,56 @@ def students(student_id):
     """
     student = db.session.query(Student).get(student_id)
     if not student:
-    	abort(500, "Ese estudiante no existe")
+        abort(500, "Ese estudiante no existe")
+
+    matching_companies = []
+    student_skills = set([skill.id for skill in student.skills])
+    companies = db.session.query(Company).all()
+
+    for company in companies:
+
+        company_skills = set([skill.id for skill in company.skills])
+        match_skills = [skill for skill in student_skills & company_skills]
+        other_skills = [skill for skill in company_skills - student_skills]
+        if len(match_skills) > 0:
+
+            # Model lists
+            match_skills_obj = [
+                db.session.query(Skill).get(skill) for skill in match_skills]
+            other_skills_obj = [
+                db.session.query(Skill).get(skill) for skill in other_skills]
+
+            match = {
+                "model": company,
+                "matches": len(match_skills),
+                "skills": match_skills_obj,
+                "other_skills": other_skills_obj
+            }
+            matching_companies.append(match)
+
+        # sort the list by matches, most matches first
+        from operator import itemgetter
+        sorted_matching_companies = sorted(matching_companies,
+                                           key=itemgetter('matches'),
+                                           reverse=True)
+
     return render_template(
         "student.html",
-        student=student
+        student=student,
+        matching_companies=sorted_matching_companies
     )
+
 
 @frontend.route("/company/<int:company_id>")
 def companies(company_id):
-	"""
-	company details
-	"""
-	company = db.session.query(Company).get(company_id)
-	if not company:
-		abort(500, "Esa compañia no existe")
+    """
+    company details
+    """
+    company = db.session.query(Company).get(company_id)
+    if not company:
+        abort(500, "Esa compañia no existe")
 
-	return render_template(
-		"company.html",
-		company=company
-	)
+    return render_template(
+        "company.html",
+        company=company
+    )
