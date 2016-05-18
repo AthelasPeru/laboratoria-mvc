9e8b1cdfc6466724f67dcf6b656921658206872d
ondoheer <ondoheer@gmail.com>
2016-05-18 12:58:10 -0500

    Cambiada funcionalidad de mathcing a modelo estudiante

--- app/blueprints/frontend.py
+++ app/blueprints/frontend.py
@@ -31,43 +31,16 @@ def students(student_id):
     """
     student = db.session.query(Student).get(student_id)
     if not student:
-        abort(500, "Ese estudiante no existe")
+        abort(500, "Ese estudiante no existe")   
 
-    matching_companies = []
-    student_skills = set([skill.id for skill in student.skills])
-    companies = db.session.query(Company).all()
-
-    for company in companies:
-
-        company_skills = set([skill.id for skill in company.skills])
-        match_skills = [skill for skill in student_skills & company_skills]
-        other_skills = [skill for skill in company_skills - student_skills]
-        if len(match_skills) > 0:
-
-            # Model lists
-            match_skills_obj = [
-                db.session.query(Skill).get(skill) for skill in match_skills]
-            other_skills_obj = [
-                db.session.query(Skill).get(skill) for skill in other_skills]
-
-            match = {
-                "model": company,
-                "matches": len(match_skills),
-                "skills": match_skills_obj,
-                "other_skills": other_skills_obj
-            }
-            matching_companies.append(match)
-
-        # sort the list by matches, most matches first
-        from operator import itemgetter
-        sorted_matching_companies = sorted(matching_companies,
-                                           key=itemgetter('matches'),
-                                           reverse=True)
+    # Retorna listado de compañias que requieren al menos 1 skill de 
+    # la alumna ordenador por ranking
+    matching_companies = student.matching_companies()
 
     return render_template(
         "student.html",
         student=student,
-        matching_companies=sorted_matching_companies
+        matching_companies=matching_companies
     )
 
 
--- app/models/student.py
+++ app/models/student.py
@@ -1,6 +1,7 @@
 from app.models import db
 from app.models.relationships import student_skills
-
+from app.models.company import Company
+from app.models.skill import Skill
 
 class Student(db.Model):
 
@@ -18,3 +19,48 @@ class Student(db.Model):
 
 	def __repr__(self):
 		return "{}, {}".format(self.name, self.email)
+
+	@property
+	def _student_skills_as_set(self):
+		"""
+		returns student skills as set to work with it
+		"""
+		return set([skill.id for skill in self.skills])
+
+	def matching_companies(self):
+		"""
+		Returns a list of matching companiesordered by relevance
+		"""
+		student_skills = self._student_skills_as_set
+
+		companies = db.session.query(Company).all()
+		matching_companies = []
+		for company in companies:
+
+		    company_skills = set([skill.id for skill in company.skills])
+		    match_skills = [skill for skill in student_skills & company_skills]
+		    other_skills = [skill for skill in company_skills - student_skills]
+		    if len(match_skills) > 0:
+
+		        # Model lists
+		        match_skills_obj = [
+		            db.session.query(Skill).get(skill) for skill in match_skills]
+		        other_skills_obj = [
+		            db.session.query(Skill).get(skill) for skill in other_skills]
+
+		        match = {
+		            "model": company,
+		            "matches": len(match_skills),
+		            "skills": match_skills_obj,
+		            "other_skills": other_skills_obj
+		        }
+		        matching_companies.append(match)
+
+		    # sort the list by matches, most matches first
+		    from operator import itemgetter
+		    sorted_matching_companies = sorted(matching_companies,
+		                                       key=itemgetter('matches'),
+		                                       reverse=True)
+		    return sorted_matching_companies
+
+
